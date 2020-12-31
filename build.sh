3cfb041b5d8010f4467c218bbb3eedb91e5a97fb
88plug <19512127+88plug@users.noreply.github.com>
2020-12-31 01:52:01 -0500

    Update build.sh
    Bump zmq from 4.2.3 to 4.3.3

--- build.sh
+++ build.sh
@@ -1,17 +1,20 @@
-apt-get update ; apt-get install -y wget
+#!/bin/bash
+#Get depends
+apt-get update ; apt-get install -y wget dotnet-sdk-2.2 git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev autoconf automake pkg-config apt-transport-https
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt-get update -y
-apt-get install apt-transport-https -y
-apt-get update -y
-apt-get -y install dotnet-sdk-2.2 git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev autoconf automake pkg-config
-#libzmq5
+
+#Install miningcore
 git clone https://github.com/coinfoundry/miningcore
 cd miningcore/src/Miningcore
 dotnet publish -c Release --framework netcoreapp2.2  -o ../../build
-wget https://github.com/zeromq/libzmq/archive/v4.2.3.tar.gz
-tar zxvf v4.2.3.tar.gz
-cd libzmq-4.2.3/
+
+#Install ZMQ
+#The default package for libzmq5 is not working by default, replaced with build of latest ZMQ.
+wget https://github.com/zeromq/libzmq/archive/v4.3.3.tar.gz
+tar zxvf v4.3.3.tar.gz
+cd libzmq-4.3.3/
 ./autogen.sh
 ./configure --prefix=/usr
 make
