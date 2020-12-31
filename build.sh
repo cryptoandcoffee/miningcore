8c4bcdc8a4924a6ec9bb81128573e880da5a942b
88plug <19512127+88plug@users.noreply.github.com>
2020-12-31 00:52:03 -0500

    Update build.sh
    ZMQ Working

--- build.sh
+++ build.sh
@@ -4,7 +4,15 @@ dpkg -i packages-microsoft-prod.deb
 apt-get update -y
 apt-get install apt-transport-https -y
 apt-get update -y
-apt-get -y install dotnet-sdk-2.2 git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5
+apt-get -y install dotnet-sdk-2.2 git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev autoconf automake pkg-config
+#libzmq5
 git clone https://github.com/coinfoundry/miningcore
 cd miningcore/src/Miningcore
 dotnet publish -c Release --framework netcoreapp2.2  -o ../../build
+wget https://github.com/zeromq/libzmq/archive/v4.2.3.tar.gz
+tar zxvf v4.2.3.tar.gz
+cd libzmq-4.2.3/
+./autogen.sh
+./configure --prefix=/usr
+make
+make install
